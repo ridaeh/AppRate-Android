0a1e8be92ffc09cd3766247ba54d6e48b08c256a
User007 <user007@breuhteam.com>
2018-03-05 14:15:37 +0100

    Coding the AppRate class for easily implement App rate Dialog

--- app/src/main/java/com/breuhteam/apprateexample/MainActivity.java
+++ app/src/main/java/com/breuhteam/apprateexample/MainActivity.java
@@ -3,11 +3,14 @@ package com.breuhteam.apprateexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.breuhteam.apprate.AppRate;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppRate.app_launched(this,"com.breuhteam.diy");
     }
 }
--- apprate/src/main/java/com/breuhteam/apprate/AppRate.java
+++ apprate/src/main/java/com/breuhteam/apprate/AppRate.java
@@ -1,8 +1,86 @@
 package com.breuhteam.apprate;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
+
 /**
  * Created by user007 on 05/03/18.
  */
 
 public class AppRate {
+
+    private final static int DAYS_UNTIL_PROMPT = 0;//Min number of days
+    private final static int LAUNCHES_UNTIL_PROMPT = 1;//Min number of launches
+
+    public static void app_launched(Context mContext,String packageName) {
+
+        SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
+
+        if (prefs.getBoolean("dontshowagain", false)) { return ; }
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // Increment launch counter
+        long launch_count = prefs.getLong("launch_count", 0) + 1;
+        editor.putLong("launch_count", launch_count);
+
+        // Get date of first launch
+        Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
+        if (date_firstLaunch == 0) {
+            date_firstLaunch = System.currentTimeMillis();
+            editor.putLong("date_firstlaunch", date_firstLaunch);
+        }
+
+        // Wait at least n days before opening
+        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
+            if (System.currentTimeMillis() >= date_firstLaunch +
+                    (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
+                showRateDialog(mContext,editor,packageName);
+            }
+        }
+
+        editor.commit();
+    }
+
+    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor, final String packageName) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        builder.setTitle(mContext.getResources().getString(R.string.rate_dialog_title));
+        builder.setMessage(mContext.getResources().getString(R.string.rate_dialog_message))
+                .setCancelable(false)
+                .setPositiveButton(mContext.getResources().getString(R.string.rate_dialog_no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Action for 'NO' Button
+                        if (editor != null) {
+                            editor.putBoolean("dontshowagain", true);
+                            editor.commit();
+                        }
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton(mContext.getResources().getString(R.string.rate_dialog_cancel),new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                })
+                .setNeutralButton(
+                        mContext.getResources().getString(R.string.rate_dialog_ok), new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+
+
+
+    }
+
+
+
 }
